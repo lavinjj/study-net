a6962bc6436abfcc4e1c33d2754e2920277c95a0
Jim Lavin <jim.lavin@ordertalk.com>
2016-01-21 09:40:43 -0600

    added question pools, fixed random question selection

--- partial/home/home.js
+++ partial/home/home.js
@@ -1,28 +1,29 @@
 /*jshint -W083 */
-angular.module('studyNet').controller('HomeCtrl',function($scope, $http){
+angular.module('studyNet').controller('HomeCtrl', function ($scope, $http) {
     $scope.currentTest = 'general';
     $scope.questions = [];
     $scope.currentQuestion = {};
     $scope.previousQuestions = [];
 
 
-    $scope.updateCurrentTest = function(value){
+    $scope.updateCurrentTest = function (value) {
         $scope.currentTest = value;
-        $http.get('data/' + value + '.json').then(function(response){
+        $http.get('data/' + value + '.json').then(function (response) {
             $scope.questions = response.data;
             $scope.previousQuestions = [];
             $scope.nextQuestion();
         });
     };
 
-    $scope.nextQuestion = function(){
-        var found = false;
+    $scope.nextQuestion = function () {
+        var found = true;
         var index = 0;
-        while(found){
+        while (found) {
+            found = false;
             index = Math.floor(Math.random() * $scope.questions.length);
-            angular.forEach($scope.previousQuestions, function(id){
-                if(id === index){
-                    found = false;
+            angular.forEach($scope.previousQuestions, function (id) {
+                if (id === index) {
+                    found = true;
                 }
             });
         }
@@ -30,7 +31,18 @@ angular.module('studyNet').controller('HomeCtrl',function($scope, $http){
         $scope.previousQuestions.push(index);
     };
 
-    $scope.init = function(){
+    $scope.isValidAnswer = function (index) {
+        var result = '';
+        var answer = $scope.currentQuestion.Answers[index];
+        if (answer) {
+            if (answer.AnswerId === $scope.currentQuestion.CorrectAnswer) {
+                result = "success";
+            }
+        }
+        return result;
+    };
+
+    $scope.init = function () {
         $scope.updateCurrentTest('general');
     };
 
